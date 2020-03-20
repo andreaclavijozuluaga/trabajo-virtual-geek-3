66c5c2c6bb1c10cc31659265bc2ac0a14e685974
Andrea Clavijo Zuluaga <andreacz96.acz@gmail.com>
2020-03-20 17:13:59 -0500

    Fundamentos Basicos de Programación!

---
+++ objetos/retoDos/retoDos.js
@@ -0,0 +1,7 @@
+const geekGirl2 = {};
+ geekGirl2.run = 'Estoy corriendo';
+ geekGirl2.runAMaraton = (distance) => `Una maraton de ${distance} kilometros`;
+
+// document.querySelector('h1').innerHTML = (geekGirl2('60'));
+
+console.log(geekGirl2(`60`));
---
+++ objetos/retoUno/retoUno.js
@@ -0,0 +1,13 @@
+const geekGirl = {};
+geekGirl.name = 'Susana';
+geekGirl.age = 34;
+geekGirl.profesion = 'Periodista';
+
+document.querySelector('.geekg1').innerHTML = `Mi nombre es ${geekGirl.name}, tengo ${geekGirl.age} años y soy ${geekGirl.profesion}`
+
+const geekGirl2 = {};
+geekGirl2.name = 'Rocío';
+geekGirl2.age = 25;
+geekGirl2.profesion = 'actriz';
+
+document.querySelector('.geekg2').innerHTML = `Mi nombre es ${geekGirl2.name}, tengo ${geekGirl2.age} años y soy ${geekGirl2.profesion}`
