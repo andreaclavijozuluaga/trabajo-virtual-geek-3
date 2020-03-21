13b078c04965b9a9b025b700a9c50ac1b34d9227
Andrea Clavijo Zuluaga <andreacz96.acz@gmail.com>
2020-03-21 01:22:31 -0500

    adelantando fundamentos basicos de programacion, intentando no caer el la desesperacion y mantener la compostura, por el sars-cov-2

---
+++ objetos/retoSeis/retoSeis.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const pet = "Dog";
+
+const milagros = { 
+    Name: 'Milu',
+    lastName: 'Clavijo',
+    age: 6,
+    hobby: 'dormir', 
+}
+
+console.log(`El nombre de mi perro es ${milagros.Name} ${milagros.lastName} su nombre real es Milagros, pero siempre le llamamos Milu, solo le digo Milagros cuando la regaño, Milu tiene ${milagros.age} años, ahora estoy preocupada que envejezca más, ella es tan gordita y hermosa quisiera que estuviera conmigo toda la vida, me encanta ver a milu ${milagros.hobby} se ve tan hermosa, a veces salto sobre ella y la abrazo`);
---
+++ objetos/retoSiete.js/retoSiete.js
@@ -0,0 +1,12 @@
+  
+'use strict';
+
+const job = "developer";
+
+const user = { 
+    firstName: 'Pepito',
+    lastName: 'Pérez',
+    age: 30,
+    job: job, }
+
+console.log(`Mi nombre es ${user.firstName} ${user.lastName}, tengo ${user.age} años y mi profesión es ${user.job}`);
